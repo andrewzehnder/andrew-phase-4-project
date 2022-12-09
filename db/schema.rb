b3811482d3e9584ea81bae162a608aedb5e301b7
Andrew <azehnder2@gmail.com>
2022-12-09 10:42:12 -0700

    Updating password column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_07_233016) do
+ActiveRecord::Schema.define(version: 2022_12_08_231010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2022_12_07_233016) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,86 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'bcrypt'
+
+puts "🌱 Seeding spices..."
+
+# Make 2 users
+# John Doe
+1.times do
+    user = User.create(name: "John Doe", username: "john-doe", password_digest: BCrypt::Password.create('Password1'))
+
+    # 1.times do
+    #     city = City.create(
+    #         name: Faker::Address.city,
+    #         country: Faker::Address.country,
+    #         population: Faker::Number.number(digits: 7)
+    #     )
+        
+    #         landmark = Landmark.create(
+    #             user_id = user.id,
+    #             city_id = city.id,
+    #             name: Faker::Mountain.name,
+    #             description: "This is the most beautiful mountain ever!",
+    #             image_URL: "https://th.bing.com/th/id/OIP.x_B30dSus6CmS4ak4BAFxwHaEK?pid=ImgDet&rs=1"
+    #         )
+    # end
+
+    # 1.times do
+    #     city = City.create(
+    #         name: Faker::Address.city,
+    #         country: Faker::Address.country,
+    #         population: Faker::Number.number(digits: 7)
+    #     )
+
+    #         landmark = Landmark.create(
+    #             user_id = user.id,
+    #             city_id = city.id,
+    #             name: Faker::Mountain.name,
+    #             description: "This is the second most beautiful mountain ever!",
+    #             image_URL: "https://th.bing.com/th/id/R.42f37c98aa9dc9fcad55a7984b61e916?rik=rEQtTRgZLS67JA&riu=http%3a%2f%2fadventure-journal.com%2fwp-content%2fuploads%2f2015%2f08%2f15645148031_0dc930716d_o.jpg&ehk=%2fqBqqew73LMjOa18V%2b56%2bkunsB9EIV8VN5mpQHtDzhk%3d&risl=&pid=ImgRaw&r=0"
+    #         )
+    # end
+end
+
+#Jane Smith
+# 1.times do
+#     user = User.create(name: "Jane Smith", username: "jane-smith", password_digest: BCrypt::Password.create('Password2'))
+
+#     1.times do
+#         city = City.create(
+#             name: Faker::Address.city,
+#             country: Faker::Address.country,
+#             population: Faker::Number.number(digits: 7)
+#         )
+        
+#             landmark = Landmark.create(
+#                 user_id = user.id,
+#                 city_id = city.id,
+#                 name: "Statue of Rock",
+#                 description: "This is the most beautiful statue ever!",
+#                 image_URL: "https://th.bing.com/th/id/R.dd59568467bd15189811b2081a7c10c9?rik=VUuMMx6EM4JgAg&pid=ImgRaw&r=0"
+#             )
+#     end
+
+#     1.times do
+#         city = City.create(
+#             name: Faker::Address.city,
+#             country: Faker::Address.country,
+#             population: Faker::Number.number(digits: 7)
+#         )
+        
+#             landmark = Landmark.create(
+#                 user_id = user.id,
+#                 city_id = city.id,
+#                 name: "Statue of Honor",
+#                 description: "This is the second most beautiful statue ever!",
+#                 image_URL: "https://th.bing.com/th/id/R.65d0f5dc351932340d1219b6a0d6235e?rik=XqhDaIHWpYyhVg&riu=http%3a%2f%2fwww.eutouring.com%2fstatues_in_paris_k15_DSC03128_lrg.jpg&ehk=TUHj5x5WNYrlPKvrlnUFD0XzALLDnpVZIfL8HNxWwqA%3d&risl=&pid=ImgRaw&r=0"
+#             )
+#     end
+
+# end
+
+
+puts "✅ Done seeding!"
