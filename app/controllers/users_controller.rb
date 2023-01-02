fa356be48a3924563cbc3b85b648adcdffd94b8a
Andrew <azehnder2@gmail.com>
2023-01-02 15:25:14 -0700

    Adding show to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,25 +3,30 @@ class UsersController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
     def create
-        user = User.create(user_params)
-        if user.valid?
-          render json: user, status: :created
-        else
-          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-        end
+      user = User.create(user_params)
+      if user.valid?
+        render json: user, status: :created
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
       end
+    end
+
+    def show
+      user = User.find(session[:user_id])
+      render json: user, status: :created
+    end
     
-      private
-    
-      def user_params
-        params.permit(:username, :password, :password_confirmation)
-      end
+    private
   
-      def render_unprocessable_entity(invalid)
-          render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
-      end
-  
-      def render_not_found
-          render json: { errors: "Not Found." }, status: :unauthorized
-      end
+    def user_params
+      params.permit(:username, :password, :password_confirmation)
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def render_not_found
+        render json: { errors: "Not Found." }, status: :unauthorized
+    end
 end
