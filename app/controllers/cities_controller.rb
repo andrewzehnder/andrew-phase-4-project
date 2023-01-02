c54506073ad4a52ccd061c2f12cfbfe00f6a1d16
Andrew <azehnder2@gmail.com>
2023-01-02 15:21:19 -0700

    Adding authorize to cities controller

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,15 @@
 class CitiesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    before_action :authorize
+
+    private
+
+    def authorize
+        return render json: { errors: ["Unauthorized"] }, status: :unauthorized unless session.include? :user_id
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
