07f7d58f5f5ac5c1fc9597baf26139d3523b0103
Andrew <azehnder2@gmail.com>
2023-01-02 16:41:42 -0700

    Pushing updated react App and Home pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
+ 
   return (
-    <div className="App">
-        <p>
-          Phase 4 Project
-        </p>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home/> } />
+        </Routes>
+     </Router>
   );
-}
+
+  }
 
 export default App;
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+const Home = ({ }) => {
+
+    return (
+      <form>
+        <h2>Cities List</h2>
+      </form>
+    )
+}
+
+export default Home;
