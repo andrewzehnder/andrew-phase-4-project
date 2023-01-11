ae2e2954c2e15d9a9b32d306f5a342300a39050c
Andrew <azehnder2@gmail.com>
2023-01-10 19:24:23 -0700

    Updating login component name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
 import './App.css';
-import LogIn from './components/LogIn';
+import LogIn from './components/Login';
 
 function App() {
 
