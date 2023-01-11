58452e5012c49fe8db2ab5a4189c16fdc9130a78
Andrew <azehnder2@gmail.com>
2023-01-10 18:42:45 -0700

    Adding a Navbar and Material UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  // useEffect(() => {
+  //   fetch("/me").then((response) => {
+  //     if (response.ok) {
+  //       response.json().then((user) => setUser(user));
+  //     }
+  //   });
+  // }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
  
   return (
     <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home/> } />
         </Routes>
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,3 @@
+//Need to add logic to the homepage to take them to the login page if they're not logged in
+//Add the ability for someone to login
+//Add the ability for someone to click a button to link out to a new page to sign up to create a new user
